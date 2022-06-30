8453ba0d91b92ccefcc5044895b36ee8e32fb32a
rajputnaman <rajputnaman2002@gmail.com>
2022-07-01 02:00:50 +0530

    deployment errors removed

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const Filter = require("bad-words");
 
 const { addUser, getUser, removeUser, getUsersInRoom } = require("./utils/users.js");
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000
 const server = http.createServer(app);
 const io = socketio(server);
 
